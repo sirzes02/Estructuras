bbbb4c3ec705d84b40703420fa1e8ef03c0bcf71
sirzes02 <sirzes02@gmail.com>
2020-08-09 00:55:52 -0500

    grafos completos

--- c++/grafos/Grafo.h
+++ c++/grafos/Grafo.h
@@ -46,4 +46,608 @@ public:
     void firstDepth(Vertex *, Vertex *);
     void firstBest(Vertex *, Vertex *);
 };
+
+void Graph::initialize()
+{
+    h = NULL;
+}
+
+bool Graph::empty()
+{
+    return !h;
+}
+
+int Graph::size()
+{
+    int cont = 0;
+    Vertex *aux = h;
+
+    while (aux)
+    {
+        cont++;
+        aux = aux->next;
+    }
+
+    return cont;
+}
+
+Vertex *Graph::getVertex(string name)
+{
+    Vertex *aux = h;
+
+    while (aux)
+    {
+        if (aux->name == name)
+        {
+            return aux;
+        }
+
+        aux = aux->next;
+    }
+
+    return NULL;
+}
+
+void Graph::insertVertex(string name)
+{
+    Vertex *newVertex = new Vertex;
+
+    newVertex->name = name;
+    newVertex->next = NULL;
+    newVertex->adjacent = NULL;
+
+    if (empty())
+    {
+        h = newVertex;
+    }
+    else
+    {
+        Vertex *aux = h;
+
+        while (aux->next)
+        {
+            aux = aux->next;
+        }
+
+        aux->next = newVertex;
+    }
+}
+
+void Graph::insertEdge(Vertex *origin, Vertex *destiny, int weight)
+{
+    Edge *newEdge = new Edge;
+    newEdge->weight = weight;
+    newEdge->next = NULL;
+    newEdge->adjacent = NULL;
+
+    Edge *aux = origin->adjacent;
+
+    if (!aux)
+    {
+        origin->adjacent = newEdge;
+        newEdge->adjacent = destiny;
+    }
+    else
+    {
+        while (aux->next != NULL)
+        {
+            aux = aux->next;
+        }
+
+        aux->next = newEdge;
+        newEdge->adjacent = destiny;
+    }
+}
+
+void Graph::adjacencyList()
+{
+    Vertex *verticeAux = h;
+    Edge *aristaAux;
+
+    while (verticeAux)
+    {
+        cout << verticeAux->name << "->";
+        aristaAux = verticeAux->adjacent;
+
+        while (aristaAux)
+        {
+            cout << aristaAux->adjacent->name << "->";
+
+            aristaAux = aristaAux->next;
+        }
+
+        verticeAux = verticeAux->next;
+
+        cout << endl;
+    }
+}
+
+void Graph::cancel()
+{
+    Vertex *aux;
+
+    while (h)
+    {
+        aux = h;
+        h = h->next;
+        delete (aux);
+    }
+}
+
+void Graph::removeEdge(Vertex *origin, Vertex *destiny)
+{
+    Edge *current = origin->adjacent;
+    bool flag = false;
+
+    if (!current)
+    {
+        cout << "The origin vertex have no edges" << endl;
+    }
+    else if (current->adjacent == destiny)
+    {
+        origin->adjacent = current->next;
+
+        delete (current);
+    }
+    else
+    {
+        Edge *before;
+
+        while (current)
+        {
+            if (current->adjacent == destiny)
+            {
+                flag = true;
+                before->next = current->next;
+                delete (current);
+                break;
+            }
+
+            before = current;
+            current = current->next;
+        }
+
+        if (!flag)
+        {
+            cout << "Those two vertices aren't connected" << endl;
+        }
+    }
+}
+
+void Graph::removeVertex(Vertex *vert)
+{
+    Vertex *current = h;
+    Edge *aux;
+
+    while (current)
+    {
+        aux = current->adjacent;
+
+        while (aux)
+        {
+            if (aux->adjacent == vert)
+            {
+                removeEdge(current, aux->adjacent);
+                break;
+            }
+
+            aux = aux->next;
+        }
+
+        current = current->next;
+    }
+
+    current = h;
+
+    if (vert == h)
+    {
+        h = h->next;
+
+        delete (current);
+    }
+    else
+    {
+
+        Vertex *before;
+
+        while (current != vert)
+        {
+            before = current;
+            current = current->next;
+        }
+
+        before->next = current->next;
+        delete (current);
+    }
+}
+
+void Graph::widthtravel(Vertex *origin)
+{
+    Vertex *current;
+    queue<Vertex *> currentQueu;
+    list<Vertex *> currentList;
+    list<Vertex *>::iterator i;
+    bool flag;
+    bool flag2;
+
+    currentQueu.push(origin);
+
+    while (!currentQueu.empty())
+    {
+        flag = false;
+        current = currentQueu.front();
+        currentQueu.pop();
+
+        for (i = currentList.begin(); i != currentList.end(); i++)
+        {
+            if (*i == current)
+            {
+                flag = true;
+            }
+        }
+
+        if (!flag)
+        {
+            cout << current->name << ", ";
+            currentList.push_back(current);
+
+            Edge *aux = current->adjacent;
+
+            while (aux)
+            {
+                flag2 = false;
+
+                for (i = currentList.begin(); i != currentList.end(); i++)
+                {
+                    if (aux->adjacent == *i)
+                    {
+                        flag2 = true;
+                    }
+                }
+
+                if (!flag2)
+                {
+                    currentQueu.push(aux->adjacent);
+                }
+
+                aux = aux->next;
+            }
+        }
+    }
+}
+
+void Graph::depthTravel(Vertex *origin)
+{
+    Vertex *current;
+    stack<Vertex *> currentStack;
+    list<Vertex *> currentList;
+    list<Vertex *>::iterator i;
+    bool flag;
+    bool flag2;
+
+    currentStack.push(origin);
+
+    while (!currentStack.empty())
+    {
+        flag = false;
+        current = currentStack.top();
+        currentStack.pop();
+
+        for (i = currentList.begin(); i != currentList.end(); i++)
+        {
+            if (*i == current)
+            {
+                flag = true;
+            }
+        }
+
+        if (!flag)
+        {
+            cout << current->name << ", ";
+            currentList.push_back(current);
+
+            Edge *aux = current->adjacent;
+
+            while (aux)
+            {
+                flag2 = false;
+
+                for (i = currentList.begin(); i != currentList.end(); i++)
+                {
+                    if (aux->adjacent == *i)
+                    {
+                        flag2 = true;
+                    }
+                }
+
+                if (!flag2)
+                {
+                    currentStack.push(aux->adjacent);
+                }
+
+                aux = aux->next;
+            }
+        }
+    }
+}
+
+void Graph::firstWidth(Vertex *origin, Vertex *destiny)
+{
+    typedef pair<Vertex *, Vertex *> VertexVertex;
+    queue<Vertex *> currentQueu;
+    stack<VertexVertex> currentStack;
+    list<Vertex *> currentList;
+    list<Vertex *>::iterator i;
+    Vertex *current;
+    Vertex *destinyActual;
+    Edge *aux;
+    bool flag;
+    bool flag2;
+    bool flag3 = false;
+
+    currentQueu.push(origin);
+
+    while (!currentQueu.empty())
+    {
+        flag = false;
+        current = currentQueu.front();
+        currentQueu.pop();
+
+        for (i = currentList.begin(); i != currentList.end(); i++)
+        {
+            if (*i == current)
+            {
+                flag = true;
+            }
+        }
+
+        if (!flag)
+        {
+            if (current == destiny)
+            {
+                flag3 = true;
+                destinyActual = destiny;
+
+                while (!currentStack.empty())
+                {
+                    cout << destinyActual->name << "<-";
+
+                    while (!currentStack.empty() && currentStack.top().second != destinyActual)
+                    {
+                        currentStack.pop();
+                    }
+
+                    if (!currentStack.empty())
+                    {
+                        destinyActual = currentStack.top().first;
+                    }
+                }
+
+                break;
+            }
+
+            currentList.push_back(current);
+
+            aux = current->adjacent;
+
+            while (aux)
+            {
+                flag2 = false;
+
+                for (i = currentList.begin(); i != currentList.end(); i++)
+                {
+                    if (aux->adjacent == *i)
+                    {
+                        flag2 = true;
+                    }
+                }
+
+                if (!flag)
+                {
+                    currentQueu.push(aux->adjacent);
+                    currentStack.push(VertexVertex(current, aux->adjacent));
+                }
+
+                aux = aux->next;
+            }
+        }
+    }
+
+    if (!flag3)
+    {
+        cout << "There is no path between these two vertices" << endl;
+    }
+}
+
+void Graph::firstDepth(Vertex *origin, Vertex *destiny)
+{
+    typedef pair<Vertex *, Vertex *> VertexVertex;
+    Vertex *currentVertex;
+    Vertex *currentDestiny;
+    stack<Vertex *> currentStack;
+    list<Vertex *> currentList;
+    stack<VertexVertex> currentStackPar;
+    list<Vertex *>::iterator i;
+    Edge *aux;
+    bool flag;
+    bool flag2;
+    bool flag3 = false;
+
+    currentStack.push(origin);
+
+    while (!currentStack.empty())
+    {
+        flag = false;
+        currentVertex = currentStack.top();
+        currentStack.pop();
+
+        for (i = currentList.begin(); i != currentList.end(); i++)
+        {
+            if (currentVertex == *i)
+            {
+                flag = true;
+            }
+        }
+
+        if (!flag)
+        {
+            if (currentVertex == destiny)
+            {
+                flag3 = true;
+                currentDestiny = destiny;
+
+                while (!currentStackPar.empty())
+                {
+                    cout << currentDestiny->name << "<-";
+
+                    while (!currentStackPar.empty() && currentStackPar.top().second != currentDestiny)
+                    {
+                        currentStackPar.pop();
+                    }
+
+                    if (!currentStackPar.empty())
+                    {
+                        currentDestiny = currentStackPar.top().first;
+                    }
+                }
+                break;
+            }
+
+            currentList.push_back(currentVertex);
+
+            aux = currentVertex->adjacent;
+
+            while (aux)
+            {
+                flag2 = false;
+
+                for (i = currentList.begin(); i != currentList.end(); i++)
+                {
+                    if (aux->adjacent == *i)
+                    {
+                        flag2 = true;
+                    }
+                }
+
+                if (!flag2)
+                {
+                    currentStack.push(aux->adjacent);
+                    currentStackPar.push(VertexVertex(currentVertex, aux->adjacent));
+                }
+            }
+        }
+    }
+
+    if (!flag3)
+    {
+        cout << "There is no path between these two vertices" << endl;
+    }
+}
+
+bool comparation(pair<Vertex *, int> a, pair<Vertex *, int> b)
+{
+    return a.second < b.second;
+}
+
+void Graph::firstBest(Vertex *origin, Vertex *destiny)
+{
+    typedef pair<Vertex *, int> costeVertex;
+    typedef pair<Vertex *, Vertex *> VertexVertex;
+    list<costeVertex> costesList;
+    list<costeVertex> orderList;
+    list<costeVertex>::iterator i;
+    list<costeVertex>::iterator j;
+    stack<VertexVertex> currentStack;
+    Vertex *currentVertex;
+    Vertex *currentDestiny;
+    Edge *aux;
+    int currentCoste = 0;
+    bool flag;
+    bool flag2 = false;
+
+    costesList.push_back(costeVertex(origin, 0));
+    orderList.push_back(costeVertex(origin, 0));
+
+    while (!orderList.empty())
+    {
+        currentVertex = orderList.front().first;
+        currentCoste = orderList.front().second;
+
+        orderList.pop_front();
+
+        if (currentVertex == destiny)
+        {
+            cout << "Coste: " << currentCoste << endl;
+
+            flag2 = true;
+            currentDestiny = destiny;
+
+            while (!currentStack.empty())
+            {
+                cout << currentDestiny->name << "<-";
+
+                while (!currentStack.empty() && currentStack.top().second != currentDestiny)
+                {
+                    currentStack.pop();
+                }
+
+                if (!currentStack.empty())
+                {
+                    currentDestiny = currentStack.top().first;
+                }
+            }
+            break;
+        }
+
+        aux = currentVertex->adjacent;
+
+        while (aux)
+        {
+            flag = false;
+            currentCoste += aux->weight;
+
+            for (i = costesList.begin(); i != costesList.end(); i++)
+            {
+                if (aux->adjacent == i->first)
+                {
+                    flag = true;
+
+                    if (currentCoste < i->second)
+                    {
+                        (*i).second = currentCoste;
+
+                        for (j = orderList.begin(); j != orderList.end(); j++)
+                        {
+                            if (j->first == aux->adjacent)
+                            {
+                                (*j).second = currentCoste;
+                            }
+                        }
+
+                        orderList.sort(comparation);
+                        currentStack.push(VertexVertex(currentVertex, aux->adjacent));
+                        currentCoste -= aux->weight;
+                    }
+                }
+            }
+
+            if (!flag)
+            {
+                costesList.push_back(costeVertex(aux->adjacent, currentCoste));
+                orderList.push_back(costeVertex(aux->adjacent, currentCoste));
+                orderList.sort(comparation);
+
+                currentStack.push(VertexVertex(currentVertex, aux->adjacent));
+                currentCoste -= aux->weight;
+            }
+
+            aux = aux->next;
+        }
+    }
+
+    if (!flag2)
+    {
+        cout << "There is no path between these two vertices" << endl;
+    }
+}
 #endif
---
+++ c++/grafos/main.cpp
@@ -0,0 +1,394 @@
+#include <iostream>
+#include <stdlib.h>
+#include "Grafo.h"
+
+using namespace std;
+
+int main()
+{
+    Graph G;
+    G.initialize();
+    int option;
+
+    G.insertVertex("afghanistan");
+    G.insertVertex("albania");
+    G.insertVertex("algeria");
+    G.insertVertex("andorra");
+    G.insertVertex("angola");
+    G.insertVertex("argentina");
+    G.insertVertex("armenia");
+    G.insertVertex("australia");
+    G.insertVertex("austria");
+    G.insertVertex("azerbaijan");
+
+    G.insertEdge(G.getVertex("afghanistan"), G.getVertex("albania"), 800);
+    G.insertEdge(G.getVertex("algeria"), G.getVertex("afghanistan"), 400);
+    G.insertEdge(G.getVertex("algeria"), G.getVertex("andorra"), 300);
+    G.insertEdge(G.getVertex("albania"), G.getVertex("andorra"), 700);
+    G.insertEdge(G.getVertex("andorra"), G.getVertex("argentina"), 900);
+    G.insertEdge(G.getVertex("andorra"), G.getVertex("armenia"), 400);
+    G.insertEdge(G.getVertex("andorra"), G.getVertex("australia"), 350);
+    G.insertEdge(G.getVertex("angola"), G.getVertex("algeria"), 500);
+    G.insertEdge(G.getVertex("angola"), G.getVertex("albania"), 450);
+    G.insertEdge(G.getVertex("angola"), G.getVertex("BJX"), 250);
+    G.insertEdge(G.getVertex("angola"), G.getVertex("australia"), 500);
+    G.insertEdge(G.getVertex("argentina"), G.getVertex("azerbaijan"), 1200);
+    G.insertEdge(G.getVertex("armenia"), G.getVertex("azerbaijan"), 450);
+    G.insertEdge(G.getVertex("australia"), G.getVertex("azerbaijan"), 450);
+    G.insertEdge(G.getVertex("australia"), G.getVertex("austria"), 650);
+    G.insertEdge(G.getVertex("austria"), G.getVertex("angola"), 650);
+
+    do
+    {
+        system("clear");
+
+        cout << "1. Insert Vertex" << endl;
+        cout << "2. Insert Edge" << endl;
+        cout << "3. Adjacent list" << endl;
+        cout << "4. Size" << endl;
+        cout << "5. Remove Vertex" << endl;
+        cout << "6. Remove Edge" << endl;
+        cout << "7. Cancel" << endl;
+        cout << "8. Width travel" << endl;
+        cout << "9. Depth travel" << endl;
+        cout << "10. First Width" << endl;
+        cout << "11. First depth" << endl;
+        cout << "12. First better" << endl;
+        cout << "13. Exit" << endl;
+        cout << endl
+             << "Choose one: ";
+        cin >> option;
+
+        switch (option)
+        {
+        case 1:
+        {
+            string name;
+
+            system("clear");
+
+            cout << "Insert the Vertex's name: ";
+            cin.ignore();
+            getline(cin, name, '\n');
+            G.insertVertex(name);
+            cin.get();
+            cin.get();
+
+            break;
+        }
+        case 2:
+        {
+            string origin, destiny;
+            int weight;
+
+            system("clear");
+
+            if (G.empty())
+            {
+                cout << "The graph is empty" << endl;
+            }
+            else
+            {
+                cout << "Insert the orign vertex name: ";
+                cin.ignore();
+                getline(cin, origin, '\n');
+                cout << "Insert the destiny vertex name: ";
+                getline(cin, destiny, '\n');
+                cout << "Insert the weight: ";
+                cin >> weight;
+
+                if (!G.getVertex(origin) || !G.getVertex(destiny))
+                {
+                    cout << "One of the vertex is invalid" << endl;
+                }
+                else
+                {
+                    G.insertEdge(G.getVertex(origin), G.getVertex(destiny), weight);
+                }
+            }
+            cin.get();
+            cin.get();
+
+            break;
+        }
+        case 3:
+        {
+            system("clear");
+
+            if (G.empty())
+            {
+                cout << "The graph is empty" << endl;
+            }
+            else
+            {
+                G.adjacencyList();
+            }
+            cin.get();
+            cin.get();
+
+            break;
+        }
+        case 4:
+        {
+            system("clear");
+
+            if (G.empty())
+            {
+                cout << "The graph is empty" << endl;
+            }
+            else
+            {
+                cout << "Size: " << G.size() << endl;
+            }
+            cin.get();
+            cin.get();
+
+            break;
+        }
+        case 5:
+        {
+            string name;
+
+            system("clear");
+
+            if (G.empty())
+            {
+                cout << "The graph is empty" << endl;
+            }
+            else
+            {
+                cout << "Insert the vertex name to remove: ";
+                cin.ignore();
+                getline(cin, name, '\n');
+
+                if (!G.getVertex(name))
+                {
+                    cout << "Invalid vertex" << endl;
+                }
+                else
+                {
+                    G.removeVertex(G.getVertex(name));
+                }
+            }
+            cin.get();
+            cin.get();
+
+            break;
+        }
+        case 6:
+        {
+            string origin, destiny;
+
+            system("clear");
+
+            if (G.empty())
+            {
+                cout << "The graph is empty" << endl;
+            }
+            else
+            {
+                cout << "Insert the origin vertex name: ";
+                cin.ignore();
+                getline(cin, origin, '\n');
+                cout << "Insert the destiny vertex name: ";
+                getline(cin, destiny, '\n');
+
+                if (!G.getVertex(origin) || !G.getVertex(destiny))
+                {
+                    cout << "Invalid vertices" << endl;
+                }
+                else
+                {
+                    G.removeEdge(G.getVertex(origin), G.getVertex(destiny));
+                }
+            }
+            cin.get();
+            cin.get();
+            break;
+        }
+        case 7:
+        {
+            system("clear");
+
+            if (G.empty())
+            {
+                cout << "The graph is empty" << endl;
+            }
+            else
+            {
+                G.cancel();
+            }
+            cin.get();
+            cin.get();
+
+            break;
+        }
+        case 8:
+        {
+            string name;
+
+            system("clear");
+
+            if (G.empty())
+            {
+                cout << "The graph is empty" << endl;
+            }
+            else
+            {
+                cout << "Insert the inicial vertice name: ";
+                cin.ignore();
+                getline(cin, name, '\n');
+
+                if (!G.getVertex(name))
+                {
+                    cout << "That vertex is invalid" << endl;
+                }
+                else
+                {
+                    G.widthtravel(G.getVertex(name));
+                }
+            }
+            cin.get();
+            cin.get();
+
+            break;
+        }
+        case 9:
+        {
+            string name;
+
+            system("clear");
+
+            if (G.empty())
+            {
+                cout << "The graph is empty" << endl;
+            }
+            else
+            {
+                cout << "Insert the inicial vertex name: ";
+                cin.ignore();
+                getline(cin, name, '\n');
+
+                if (!G.getVertex(name))
+                {
+                    cout << "That vertex is invalid" << endl;
+                }
+                else
+                {
+                    G.depthTravel(G.getVertex(name));
+                }
+            }
+            cin.get();
+            cin.get();
+
+            break;
+        }
+        case 10:
+        {
+            string origin, destiny;
+
+            system("clear");
+
+            if (G.empty())
+            {
+                cout << "The graph is empty" << endl;
+            }
+            else
+            {
+                cout << "Insert the origin vertex name: ";
+                cin.ignore();
+                getline(cin, origin, '\n');
+                cout << "Insert the destiny vertex name: ";
+                getline(cin, destiny, '\n');
+
+                if (!G.getVertex(origin) || !G.getVertex(destiny))
+                {
+                    cout << "Invalid vertices" << endl;
+                }
+                else
+                {
+                    G.firstWidth(G.getVertex(origin), G.getVertex(destiny));
+                }
+            }
+            cin.get();
+            cin.get();
+
+            break;
+        }
+        case 11:
+        {
+            string origin, destiny;
+
+            system("clear");
+
+            if (G.empty())
+            {
+                cout << "The graph is empty" << endl;
+            }
+            else
+            {
+                cout << "Insert the origin vertex name: ";
+                cin.ignore();
+                getline(cin, origin, '\n');
+                cout << "Insert the destiny vertex name: ";
+                getline(cin, destiny, '\n');
+
+                if (!G.getVertex(origin) || !G.getVertex(destiny))
+                {
+                    cout << "Invalid vertices" << endl;
+                }
+                else
+                {
+                    G.firstDepth(G.getVertex(origin), G.getVertex(destiny));
+                }
+            }
+            cin.get();
+            cin.get();
+
+            break;
+        }
+        case 12:
+        {
+            string origin, destiny;
+
+            system("clear");
+
+            if (G.empty())
+            {
+                cout << "The graph is empty" << endl;
+            }
+            else
+            {
+                cout << "Insert the origin vertex name: ";
+                cin.ignore();
+                getline(cin, origin, '\n');
+                cout << "Insert the destiny vertex name: ";
+                getline(cin, destiny, '\n');
+
+                if (!G.getVertex(origin) || !G.getVertex(destiny))
+                {
+                    cout << "Invalid vertices" << endl;
+                }
+                else
+                {
+                    G.firstBest(G.getVertex(origin), G.getVertex(destiny));
+                }
+            }
+            cin.get();
+            cin.get();
+
+            break;
+        }
+        case 13:
+        {
+            break;
+        }
+        default:
+        {
+            cout << "Error, choose a correct option" << endl;
+        }
+        }
+    } while (option != 13);
+
+    return EXIT_SUCCESS;
+}
